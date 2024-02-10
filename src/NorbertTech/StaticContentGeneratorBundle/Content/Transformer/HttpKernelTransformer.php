c400d5f8e7a0c70b614475835885467a4df2630b
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2024-02-10 21:24:50 +0100

    Properly respect router.request_context settings (#444)
    * Properly respect router.request_context settings
    
    * Fixed failing tests

--- src/NorbertTech/StaticContentGeneratorBundle/Content/Transformer/HttpKernelTransformer.php
+++ src/NorbertTech/StaticContentGeneratorBundle/Content/Transformer/HttpKernelTransformer.php
@@ -8,16 +8,16 @@ use NorbertTech\StaticContentGeneratorBundle\Content\Content;
 use NorbertTech\StaticContentGeneratorBundle\Content\Source;
 use NorbertTech\StaticContentGeneratorBundle\Content\Transformer;
 use Symfony\Component\HttpKernel\HttpKernelBrowser;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 final class HttpKernelTransformer implements Transformer
 {
-    private HttpKernelInterface $kernel;
+    private KernelInterface $kernel;
 
     private RouterInterface $router;
 
-    public function __construct(HttpKernelInterface $kernel, RouterInterface $router)
+    public function __construct(KernelInterface $kernel, RouterInterface $router)
     {
         $this->kernel = $kernel;
         $this->router = $router;
@@ -26,7 +26,16 @@ final class HttpKernelTransformer implements Transformer
     public function transform(Source $source) : Content
     {
         $kernelBrowser = new HttpKernelBrowser($this->kernel);
-        $kernelBrowser->request('GET', $path = $this->router->generate($source->routerName(), $source->parameters()));
+        $kernelBrowser->request(
+            'GET',
+            $path = $this->router->generate($source->routerName(), $source->parameters()),
+            [],
+            [],
+            [
+                'HTTP_HOST' => $this->kernel->getContainer()->getParameter('router.request_context.host') ?? 'localhost',
+                'HTTPS' => $this->kernel->getContainer()->getParameter('router.request_context.scheme')  === 'https',
+            ]
+        );
 
         if ($kernelBrowser->getResponse()->getStatusCode() > 299) {
             throw new \RuntimeException('Can\'t generate static content for route ' . $source->routerName());
